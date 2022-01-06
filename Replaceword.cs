31a7541da2b2ca5eafd8222c36744fad5d68bb64
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-01-06 16:49:01 +0530

    relaceword

---
+++ Replaceword.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Replaceword
+    {
+        public static void ReadingInputFromUser()
+        {
+            Console.WriteLine(" please enter your UserName");
+            string name=Console.ReadLine();
+            if(name.Length>=3)
+            {
+                ReplaceStringWithTemplate(name);
+            }
+            else
+            {
+                Console.WriteLine("please provide atleast 3 characters");
+            }
+        }
+        public static void ReplaceStringWithTemplate(string name)
+        {
+            string template = "Hello <<userName>>,How are you?";
+            if(template.Contains("<<userName>>"))
+            {
+                string result = template.Replace("<<userName>>", name);
+                Console.WriteLine("after Replace :" + result);
+             
+            }
+            else
+            {
+                Console.WriteLine( "userName not exists");
+            }
+        }
+     }
+}
